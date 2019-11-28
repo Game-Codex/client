77b92e22af535a8588c5b2d42bac1b2c73ee3873
zoe-zachary <zoezachary27@gmail.com>
2019-11-28 21:29:50 +0700

    savepoint

---
+++ js/pokemon.js
@@ -0,0 +1,39 @@
+const currentCards = []
+
+function fillCurrentCards(e, number) {
+   e.preventDefault()
+   console.log(number)
+   // $.ajax({
+
+   //    method: 'get',
+   //    url: 'http://localhost:3000/pokemon/cards'
+   // })
+}
+
+function pokemonPopulate() {
+
+   $.ajax({
+
+      method: 'get',
+      url: 'http://localhost:3000/pokemon/cards'
+   })
+   .done((cards) => {
+      
+      for(let i in cards) {
+
+         $('#pokemon-card-board').append(`
+   
+            <div class="pokemon-card">
+            
+               <a href="${cards[i].imageUrlHiRes}">
+
+                  <img src="${cards[i].imageUrl}" alt="${cards[i].name}">
+               </a>
+
+               <h5 class="pokemon-card-name">${cards[i].name}</h5>
+            </div>
+         `)
+      }
+   })
+   .fail(error => console.log(error))
+}
